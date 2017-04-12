9da7f4b7e1d8b2ed5c721938f9a045e2c056d95c
Freddy Fallon <freddyfallon@gmail.com>
2017-04-12 10:11:29 +0100

    Add error when taking off an plane is landed

--- src/plane.js
+++ src/plane.js
@@ -5,12 +5,15 @@ function Plane() {
 
 
 Plane.prototype.takeOff = function () {
-  if ( this.isFlying == true ) {
+  if ( this.isFlying === true ) {
     throw "Plane already flying";
   };
   this.isFlying = true;
 };
 
 Plane.prototype.land = function() {
+  if ( this.isFlying === false ) {
+    throw "Plane already landed";
+  };
   this.isFlying = false;
 };
